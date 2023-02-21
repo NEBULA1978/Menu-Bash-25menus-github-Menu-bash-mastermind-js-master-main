50376db4d2e5c7787ec68d8f256944c58a66b66b
NEBULA1978 <rpascualey@gmail.com>
2023-02-21 20:42:02 +0100

    AÑadiendo input a las variables

--- bloque1/01variables.js
+++ bloque1/01variables.js
@@ -5,6 +5,38 @@ var cajaC = cajaA + cajaB;
 
 console.log(cajaA, cajaB, cajaC);
 
+// Pedimos al usuario que introduzca el valor de la variable A
+var valorA = prompt("Introduce el valor de la variable A:");
+
+// Verificamos si el valor es un número
+while (isNaN(valorA)) {
+  // Si el valor no es un número, pedimos al usuario que lo introduzca de nuevo
+  valorA = prompt("Introduce un valor numérico para la variable A:");
+}
+
+// Convertimos el valor de la variable A a un número
+var numeroA = parseFloat(valorA);
+
+// Pedimos al usuario que introduzca el valor de la variable B
+var valorB = prompt("Introduce el valor de la variable B:");
+
+// Verificamos si el valor es un número
+while (isNaN(valorB)) {
+  // Si el valor no es un número, pedimos al usuario que lo introduzca de nuevo
+  valorB = prompt("Introduce un valor numérico para la variable B:");
+}
+
+// Convertimos el valor de la variable B a un número
+var numeroB = parseFloat(valorB);
+
+// Calculamos el valor de la variable C como la suma de las variables A y B
+var numeroC = numeroA + numeroB;
+
+// Mostramos los valores de las variables por consola
+console.log(numeroA, numeroB, numeroC);
+
+// OTRO CODIGO
+
 var x = 2;
 var y = 3;
 
