961f37a7cce575e34fb1e68ed18d66d91002f99a
NEBULA1978 <rpascualey@gmail.com>
2023-02-21 21:48:41 +0100

    Añadiendo ejemplos bloque1 vectores

--- bloque1/04tiposdatos.js
+++ bloque1/04tiposdatos.js
@@ -1,55 +1,63 @@
-// Qué son los tipos
-var suma = 2 + '2'; //'22'
-
-// Los tipos de javascript:
+// Definición de variables con diferentes tipos de datos
 var numero = 2;
 var string = 'Soy un texto';
 var booleano = true;
 
+// Array y objeto
 var array = ["aa", "bb", 3, [1, 2]];
 var objeto = {
-    nombre: 'Martin', 
+    nombre: 'Martin',
     apellido: 'Cristobal',
     edad: 26
 }
 
-var nombreFuncion = function() {}
-// Hablaremos de las funciones mas adelante
+// Definición de una función anónima
+var nombreFuncion = function () { }
 
 // null y undefined
-// null NO tengo definido un valor
-var valorNulo = null;
-var valorUndefined;
-console.log(typeof valorNulo, typeof valorUndefined);
+var valorNulo = null; // Este valor se utiliza cuando no tengo definido un valor
+var valorUndefined; // Este valor se utiliza para declarar una variable sin inicializarla
 
-valorUndefined = 5;
-// los tipos son dinamicos
+// Imprime el tipo de la variable (string, number, boolean, etc.)
+console.log(typeof booleano); // 'boolean'
 
-// typeof
-console.log(typeof booleano == 'string')
+// Tipos de datos dinámicos
+// En Javascript, el tipo de una variable puede cambiar en tiempo de ejecución:
+var suma = 2 + '2'; // El resultado de la suma es '22', que es un string.
 
-
-
-
-
-
-
-
+// Ejemplo de uso de array
 var vector = [];
 vector.push(3);
 vector.push(4);
 vector.pop();
+console.log(vector); // El resultado es [3], ya que se eliminó el último elemento (4).
 
+// Ejemplo de uso de objeto
+var objetoParaProbar = { nombre: 'Martin' };
+objetoParaProbar.apellido = 'Cristobal'; // Agrega un atributo 'apellido' con valor 'Cristobal'
+console.log(objetoParaProbar); // El resultado es { nombre: 'Martin', apellido: 'Cristobal' }
+
+
+// Declaramos un arreglo vacío
+var vector = [];
+
+// Agregamos los elementos 3 y 4 al arreglo
+vector.push(3);
+vector.push(4);
+
+// Eliminamos el último elemento del arreglo (en este caso, 4)
+vector.pop();
+
+// Mostramos el arreglo actual
 console.log(vector);
 
-var objetoParaProbar = {nombre: 'Martin'};
-objetoParaProbar[apellido] = 'Cristobal';
+// Aquí hay algunos otros métodos útiles que puedes usar con arreglos:
 
+// array.length: devuelve el número de elementos en el arreglo.
+//     array.concat(array2): combina dos arreglos en uno solo.
+//         array.slice(inicio, fin): devuelve una porción del arreglo, desde el índice "inicio" hasta el índice "fin"(sin incluir "fin").
+//             array.splice(indice, cantidad, elementos): elimina o inserta elementos en el arreglo.El parámetro "indice" indica la posición donde comenzar a hacer cambios, "cantidad" indica cuántos elementos eliminar a partir de ese índice, y "elementos" son los elementos que se insertarán en esa posición.
+//                 array.reverse(): invierte el orden de los elementos en el arreglo.
+//                     array.sort(): ordena los elementos del arreglo de manera ascendente(para ordenar de manera descendente, use array.reverse() después de array.sort()).
 
-
-
-
-
-
-
-
+// Estos son solo algunos de los métodos más comunes; hay muchos otros que puedes encontrar en la documentación oficial de JavaScript.
--- bloque1/05vectoresyobjectos.js
+++ bloque1/05vectoresyobjectos.js
@@ -1,6 +1,6 @@
-var array = ["aa", "bb", 3, [1, 2], {nombre: 'Nombre', apellido: 'Apellido'}];
+var array = ["aa", "bb", 3, [1, 2], { nombre: 'Nombre', apellido: 'Apellido' }];
 var objeto = {
-    nombre: 'Martin', 
+    nombre: 'Martin',
     apellido: 'Cristobal',
     edad: 26,
     coches: ['Ferrari', 'Lamborghini'],
@@ -10,22 +10,26 @@ var objeto = {
     }
 }
 
+// Acceso a elementos de un array
 var item0 = array[0];
 var item1 = array[1];
 
 var indice = 4;
-var item4 = array[indice].nombre;
+var item4 = array[indice].nombre; // Acceso a un objeto anidado dentro del array
 
+// Acceso a elementos de un objeto
 var nombreObjeto = objeto.nombre;
-var textoCalle = objeto.coches[0];
+var textoCalle = objeto.coches[0]; // Acceso a un elemento de un array dentro del objeto
 
-
-var item50 = array[50];
+// Acceso a posiciones no definidas
+var item50 = array[50]; // Devolverá undefined, ya que el array no tiene tantos elementos
 console.log(item50);
 
-var piso = objeto.piso[1];
+var piso = objeto.piso[1]; // Devolverá undefined, ya que el objeto no tiene una propiedad 'piso'
 console.log(piso);
 
-var longitud = array.length
+// Obtener la longitud de un array
+var longitud = array.length;
 
-var isValidPosition = 50 >= 0 && 50 < array.length
+// Verificar si una posición dada está dentro de los límites del array
+var isValidPosition = 50 >= 0 && 50 < array.length;
